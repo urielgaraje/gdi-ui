832069932bfd1e2f3fe55582d18d2f1df77d863a
Uriel Blanco <uri92_el@hotmail.com>
2025-02-18 00:33:37 +0100

    feat: add variables from tokens

--- build-tokens.js
+++ build-tokens.js
@@ -5,26 +5,14 @@ import {
   logBrokenReferenceLevels,
   logWarningLevels,
   logVerbosityLevels,
+  formats,
 } from 'style-dictionary/enums';
-
-StyleDictionary.registerTransform({
-  name: 'value/add-global-prefix',
-  type: 'value',
-  transitive: true,
-  filter: function (token) {
-    debugger;
-    console.log({ token });
-    return typeof token.value === 'string' && token.value.includes('{') && !token.value.includes('{global.');
-  },
-  transform: function (token) {
-    // Agrega "global." a las referencias de color, space y radius
-    return token.value.replace(/{(color|space|radius)/g, '{global.$1');
-  },
-});
 
 // will register them on StyleDictionary object
 // that is installed as a dependency of this package.
-register(StyleDictionary);
+register(StyleDictionary, {
+  excludeParentKeys: true,
+});
 
 const sd = new StyleDictionary({
   // make sure to have source match your token files!
@@ -33,13 +21,13 @@ const sd = new StyleDictionary({
   preprocessors: ['tokens-studio'], // <-- since 0.16.0 this must be explicit
   platforms: {
     css: {
-      transformGroup: 'tokens-studio', // <-- apply the tokens-studio transformGroup to apply all transforms
-      transforms: ['name/kebab', 'value/add-global-prefix'], // <-- add a token name transform for generating token names, default is camel
+      transformGroups: 'token-studio',
+      transforms: ['name/kebab'], // <-- add a token name transform for generating token names, default is camel
       buildPath: 'src/themes/',
       files: [
         {
           destination: 'variables.css',
-          format: 'css/variables',
+          format: formats.cssVariables,
         },
       ],
     },
@@ -53,5 +41,5 @@ const sd = new StyleDictionary({
   },
 });
 
-await sd.cleanAllPlatforms();
-await sd.buildAllPlatforms();
+/* await sd.cleanAllPlatforms();
+await sd.buildAllPlatforms(); */
