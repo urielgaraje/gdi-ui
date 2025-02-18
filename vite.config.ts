2f0baf452ea93beccdacfad3c74b28ada840409d
Uriel Blanco <uri92_el@hotmail.com>
2025-02-18 20:05:00 +0100

    refactor: add default theme into pkg

--- vite.config.ts
+++ vite.config.ts
@@ -27,15 +27,17 @@ export default defineConfig({
     rollupOptions: {
       external: ['react', 'react-dom', 'react/jsx-runtime'],
       input: Object.fromEntries(
-        globSync(['src/components/**/index.{ts,tsx}', 'src/main.{ts,tsx}']).map((file) => {
-          // This remove `src/` as well as the file extension from each
-          // file, so e.g. src/nested/foo.js becomes nested/foo
-          const entryName = path.relative('src', file.slice(0, file.length - path.extname(file).length));
-          // This expands the relative paths to absolute paths, so e.g.
-          // src/nested/foo becomes /project/src/nested/foo.js
-          const entryUrl = fileURLToPath(new URL(file, import.meta.url));
-          return [entryName, entryUrl];
-        })
+        globSync(['src/components/**/index.{ts,tsx}', 'src/main.{ts,tsx}', 'src/themes/default-theme.css']).map(
+          (file) => {
+            // This remove `src/` as well as the file extension from each
+            // file, so e.g. src/nested/foo.js becomes nested/foo
+            const entryName = path.relative('src', file.slice(0, file.length - path.extname(file).length));
+            // This expands the relative paths to absolute paths, so e.g.
+            // src/nested/foo becomes /project/src/nested/foo.js
+            const entryUrl = fileURLToPath(new URL(file, import.meta.url));
+            return [entryName, entryUrl];
+          }
+        )
       ),
       output: {
         entryFileNames: '[name].[format].js',
